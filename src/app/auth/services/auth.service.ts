75c9a551616fab168856a4513b9e96396dd35589
Younes Makhchan <younessmakhchane@gmail.com>
2024-03-07 21:37:57 +0100

    adding the  service for login

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
+const BASE_URL=["http://localhost:9000"]//change to env
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  //normal signuprequest should be an interface
+  register(signUpRequest:any):Observable<any>{
+    return this.http.post(`${BASE_URL}/api/auth/signup`,signUpRequest)
+  }
 }
