3a2053b9bac490da0fe2a8fc8cbcfc6943e83e41
Younes Makhchan <younessmakhchane@gmail.com>
2024-03-07 21:37:03 +0100

    fixing styling

--- src/app/auth/components/signup/signup.component.ts
+++ src/app/auth/components/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-
+import { AuthService } from '../../services/auth.service';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -9,15 +11,16 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 export class SignupComponent implements OnInit{
   isSpinning:boolean =false;
   signupForm!:FormGroup;
-  constructor(private fb:FormBuilder) { }
+  constructor(private fb:FormBuilder, private authService:AuthService,private message:NzMessageService,private router:Router) { }
   ngOnInit(): void {
     this.signupForm=this.fb.group({
       name:[null,Validators.required],
       email:[null,[Validators.required,Validators.email]],
-      password:[null,[Validators.required,this.passwordPaternValidator]],
+      password:[null,[Validators.required]],
       confirmPassword:[null,[Validators.required,this.confirmationValidator]]
     })
   }
+  //should be moved to a custom validator helper
   confirmationValidator=(control:FormControl):{[s:string]:boolean}=>{
 
     if(!control.value){
@@ -69,6 +72,18 @@ export class SignupComponent implements OnInit{
   }
 
   submit(){
-    console.log(this.signupForm.value);
+    this.authService.register(this.signupForm.value).subscribe({
+      next:(res)=>{
+              if(res.id !=null){
+                this.message.success("Signup successfull",{nzDuration:5000});
+                this.router.navigate(['/login']);
+              }else{
+                this.message.error("Signup failed",{nzDuration:5000});
+              }
+      },
+      error:(error:any)=>{
+        this.message.error("Signup failed",{nzDuration:5000});
+      },
+    })
   }
 }
